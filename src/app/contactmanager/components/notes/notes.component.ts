1f18018eec88d4fad19a4ee463349855bcf436b7
Shane Reeve <shanereeve@gmail.com>
2022-08-01 17:47:45 -0600

    Adding Pagination and Filtering

--- src/app/contactmanager/components/notes/notes.component.ts
+++ src/app/contactmanager/components/notes/notes.component.ts
@@ -1,4 +1,10 @@
-import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
@@ -7,26 +13,28 @@ import { Note } from '../../models/note';
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
-  styleUrls: ['./notes.component.scss']
+  styleUrls: ['./notes.component.scss'],
 })
-export class NotesComponent implements OnInit, AfterViewInit  {
-
-
-  @Input() notes: Note[]
+export class NotesComponent implements OnInit, AfterViewInit {
+  @Input() notes: Note[];
   // @Input() notes: Note[];
 
   displayedColumns: string[] = ['position', 'title', 'date'];
-  dataSource: MatTableDataSource<Note>
+  dataSource: MatTableDataSource<Note>;
 
   // displayedColumns: string[] = ['position', 'title', 'date' ];
   // dataSource: MatTableDataSource<Note>;
 
-  constructor() { }
+  constructor() {}
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   // @ViewChild(MatPaginator) paginator: MatPaginator;
   // @ViewChild(MatSort) sort: MatSort;
 
   ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+
     // this.dataSource.paginator = this.paginator;
     // this.dataSource.sort = this.sort;
   }
@@ -36,9 +44,13 @@ export class NotesComponent implements OnInit, AfterViewInit  {
     // this.dataSource = new MatTableDataSource<Note>(this.notes);
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
   // applyFilter(event: Event) {
   //   const filterValue = (event.target as HTMLInputElement).value;
   //   this.dataSource.filter = filterValue.trim().toLowerCase();
   // }
-
 }
